904663ec329ca3b10bdec933e0b988504a675289
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-27 23:10:29 -0300

    Initial commit

---
+++ tasks/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
