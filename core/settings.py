b1cbf6de51ce3622abc81d0b35d93afc43bca1f5
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-28 20:05:22 -0300

    Update settings.py
    Adição do domínio do frontend permitido nas configurações do CORS

--- core/settings.py
+++ core/settings.py
@@ -191,6 +191,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173", 
     "http://localhost:3000", 
     "http://127.0.0.1:3000", 
+    "https://labce-lista-dinamica.vercel.app/",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
