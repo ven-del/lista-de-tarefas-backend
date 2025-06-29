d2b046c554be91a8568babdd1621a1deaa16a74f
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-28 21:32:47 -0300

    Update build.sh
    Adicionada a criação de um superuser para acessar o admin do Django em produção.
    
    O Render free não permite o acesso do Shell para a criação de forma segura.

--- build.sh
+++ build.sh
@@ -10,3 +10,22 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+# Você que tá vendo isso, essa senha é apenas para testes. 
+# Em produção, use uma senha mais forte e segura. 
+# Este é um ambiente controlado de aprendizado, um laboratório.
+# Nunca crie um superuser no script de deploy.
+python manage.py shell -c "
+from users.models import User
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser(
+        email='vendel@labce.com',
+        password='SenhaAdmin123!',
+        name='Vendel Admin'
+    )
+    print('✅ Superuser criado com sucesso!')
+    print('📧 Email: vendel@labce.com')
+    print('🔑 Senha: SenhaAdmin123!')
+else:
+    print('ℹ️ Superuser já existe no banco.')
+"
