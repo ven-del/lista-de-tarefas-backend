cdcd0ee25956d2a00bb745b3eba045a9fb9b2d84
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-28 22:06:48 -0300

    feat: Exibir a lista de tasks no admin
    Agora, ao acessar o admin, é possível ver as tasks dos usuários que foram criadas e fazer a filtrage,

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'completed', 'created_at']
+    list_filter = ['completed', 'created_at', 'user']
+    search_fields = ['title', 'description', 'user__name', 'user__email']
+    ordering = ['-created_at']
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'user', 'completed')
+        }),
+        ('Datas', {
+            'fields': ('created_at', 'updated_at'),
+        }),
+    )
+    
+    readonly_fields = ['created_at', 'updated_at']
+    
